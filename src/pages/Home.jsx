f6d32acba637c2ad519bbc33d1728a2ae6f715cb
(es)이지현 <es-akis04@hybecorp.com>
2025-09-08 12:27:41 +0900

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import { useNavigate } from 'react-router-dom';
+
+function Home() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="home-container">
+      <img src="img/banner.png" className="banner-image"></img>
+      <p>똑똑👀</p>
+      <button onClick={() => navigate('/test/about')}>1.🎁</button>
+      <button onClick={() => navigate('/test/present')}>2. 🎈</button>
+      <button onClick={() => navigate('/test/letter')}>3. 💌</button>
+    </div>
+  );
+}
+
+export default Home;
